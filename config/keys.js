5982b94b6efec6ae16ec8a097cbcd047994aaa5b
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-06 19:51:01 +0530

    Changed db name

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:"mongodb+srv://pika:123@cluster1.1ajyi.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://pika:123@cluster1.1ajyi.gcp.mongodb.net/mern_shoppinglist?retryWrites=true&w=majority"
 }
 // Root Username - pika, password - 123
